61cc69b527ae10464d6e720c016b2bff43827e37
nmjean-pierr <157131777+nmjean-pierr@users.noreply.github.com>
2024-01-19 08:11:39 -0600

    Add files via upload
    This is an unfinished program.

---
+++ my list.py
@@ -0,0 +1,20 @@
+#Name
+#1/12
+#Lists
+
+#Initialize
+#mySchedule stores a list of the classes you are currently taking at Jones and your lunch period as strings
+mySchedule = []
+
+#Main
+
+#Complete the following tasks using list/array methods. Print the result for each task.
+
+#Task 1: Add your 1st - 7th period classes to the list in order (index 0 should be your 1st period class)
+
+#Each item on a list has an index(0,1,2,3, etc)
+#Task 2: Print your 3rd period class
+
+#Task 3: Add your lunch period in between your 3rd and 4th period
+
+#Final Task: Remove your least favorite class
